a79d88319b91636ab63a1f80761aa2503ce319cc
Wassim AZIRAR <wassim.azirar@gmail.com>
2021-11-20 12:40:25 +0100

    move files

--- pluralsight/scraping-first-web-page-python/scrapy_projects/ItemSpider/ItemSpider/items.py
+++
@@ -1,7 +0,0 @@
-import scrapy
-
-class CompanyDetailsItem(scrapy.Item):
-    company_name = scrapy.Field()
-    company_price_intraday = scrapy.Field()
-    company_symbol = scrapy.Field()
-    company_symbol_link = scrapy.Field()
--- pluralsight/scraping-first-web-page-python/scrapy_projects/PipelineSpider/PipelineSpider/items.py
+++
@@ -1,5 +0,0 @@
-import scrapy
-
-class CompanyDetailsItem(scrapy.Item):
-    company_name = scrapy.Field()
-    company_price_intraday = scrapy.Field()
