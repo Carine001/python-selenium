e4a932d36d4bb1adc92827c9f08b58df2870fd74
Carine <carine.doucoure@gmail.com>
2021-11-25 17:38:59 +0100

    project finished

--- scripts/telemis.py
+++ scripts/telemis.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 # get chrome driver
 chrome_service = ChromeService(r"..\drivers\chromedriver.exe")
@@ -22,14 +24,41 @@ driver = webdriver.Chrome(service=chrome_service, options=options)
 driver.get("https://be-test22:8443/site/")
 time.sleep(1)
 
-# login = driver.find_element(By.XPATH, "//button[contains(text(), 'Login')]")
-# login.click()
-# time.sleep(1)
+# username
+username = driver.find_element(By.ID, "username")
+username.send_keys("radio")
 
-# signup = driver.find_element(By.XPATH, "//button[contains(text(), 'Sign up')]")
-# signup.click()
-# time.sleep(1)
+# password
+password = driver.find_element(By.ID, "password")
+password.send_keys("radio")
 
-# signup = driver.find_element(By.XPATH, "//button[contains(text(), 'Register with an email')]")
-# signup.click()
-# time.sleep(1)
+login = driver.find_element(By.NAME, "login")
+login.click()
+
+dicomgate = driver.find_element(By.ID, "mat-expansion-panel-header-6")
+dicomgate.click()
+
+selectdicom = driver.find_element(By.LINK_TEXT, "Dicom gate")
+selectdicom.click()
+
+WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, "iframe[src^='https://be-test22:9920']")))
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "btn-success"))).click()
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "id"))).send_keys("test")
+
+# WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "div[.//span[contains(text(), 'STORE')]]"))).click()
+
+# WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "//mat-select[name = 'scope']"))).click()
+# WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "mat-checkbox-2-input"))).click()
+
+# name = driver.find_element(By.NAME, "id")
+# password.send_keys("test")
+
+# search = driver.find_element(By.XPATH, "//input[@name='query']")
+# search.send_keys("Jean")
+# search.send_keys(Keys.RETURN)
+
+
+# leave the iframe
+driver.switch_to.default_content()
+time.sleep(3)
+driver.quit()
